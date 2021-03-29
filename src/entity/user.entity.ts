bab2335a53c08c082f5210487b7afa3404d09519
hu hao <huhao@hus-MacBook-Pro.local>
2021-03-29 19:25:20 +0800

    [feat] 模拟用户信息的数据

---
+++ src/entity/user.entity.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-03-29 15:57:04
+ * @LastEditTime: 2021-03-29 19:19:00
+ * @LastEditors: Stevie
+ * @Description:
+ * @FilePath: /react-learning-demo/src/entity/user.entity.ts
+ */
+export interface IUser {
+  name: string;
+  gender: string;
+  age: number;
+  birthday: Date;
+  city: string;
+  zipcode: string;
+}
--- src/mock/user.ts
+++ src/mock/user.ts
@@ -2,7 +2,7 @@
  * @Author: Stevie
  * @Date: 2021-03-28 17:59:14
  * @LastEditors: Stevie
- * @LastEditTime: 2021-03-28 20:40:04
+ * @LastEditTime: 2021-03-29 19:21:23
  * @Description: file content
  */
 import Mock from 'mockjs'
@@ -10,11 +10,13 @@ import { get } from '../utils/http.service'
 
 const URL_USER = 'http://api/mock/user.com';
 Mock.mock(URL_USER, {
-    "user|10": [{                                  //10组数据
-        'name': '@cname',                    //中文名称
-        'age|1-100': 100,                       //100以内随机整数
-        'birthday': '@date("yyyy-MM-dd")',  //日期
-        'city': '@city(true)'                   //中国城市
+    "user|3": [{                            //10组数据
+        'name': '@cname',                   //中文名称
+        'gender': '@cword("男女")',          //性别随机男或女       
+        'age|1-100': 100,                    //100以内随机整数
+        'birthday': '@date("yyyy-MM-dd")',   //日期
+        'city': '@city(true)',               //中国城市
+        "zipcode": "@zip"                        //邮政编码
     }]
 });
 
