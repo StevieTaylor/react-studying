a425942d69d82be48403539a475460d2874b3aab
steviehao <gahao111@outlook.com>
2021-03-29 11:26:32 +0800

    [feat] 引入mockjs生成mock数据

---
+++ src/mock/config.ts
@@ -0,0 +1,16 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-03-28 18:31:13
+ * @LastEditors: Stevie
+ * @LastEditTime: 2021-03-28 18:32:47
+ * @Description: file content
+ */
+import Mock from 'mockjs'
+
+Mock.setup({
+    timeout: 400
+})
+
+Mock.setup({
+    timeout: '200-600'
+})
--- src/mock/user.ts
+++ src/mock/user.ts
@@ -2,7 +2,7 @@
  * @Author: Stevie
  * @Date: 2021-03-28 17:59:14
  * @LastEditors: Stevie
- * @LastEditTime: 2021-03-28 18:25:02
+ * @LastEditTime: 2021-03-28 20:40:04
  * @Description: file content
  */
 import Mock from 'mockjs'
@@ -13,7 +13,7 @@ Mock.mock(URL_USER, {
     "user|10": [{                                  //10组数据
         'name': '@cname',                    //中文名称
         'age|1-100': 100,                       //100以内随机整数
-        'birthday': '@date("yyyy-MM-dd")',      //日期
+        'birthday': '@date("yyyy-MM-dd")',  //日期
         'city': '@city(true)'                   //中国城市
     }]
 });
