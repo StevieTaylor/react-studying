b4174930b9a598c174b174587255979cae0dc49a
steviehao <gahao111@outlook.com>
2021-03-29 11:27:47 +0800

    [feat] Props

---
+++ src/pages/Props/PropTypes.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PropTypesLimit extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+PropTypesLimit.propTypes = {
+
+}
+
+export default PropTypesLimit
--- src/pages/Props/index.tsx
+++ src/pages/Props/index.tsx
@@ -1,15 +1,18 @@
 /*
  * @Author: Stevie
  * @Date: 2021-03-27 20:56:55
- * @LastEditTime: 2021-03-27 21:07:55
+ * @LastEditTime: 2021-03-29 11:27:12
  * @LastEditors: Stevie
  * @Description:
- * @FilePath: /react-learning-demo/src/pages/Props/index.tsx
+ * @FilePath: /react-learning-demo/src/pages/PropsDemo/index.tsx
  */
 import React from "react"
-
+import { getUser } from '../../mock/user'
 class Props extends React.Component {
   render() {
+    getUser().then((res) => {
+      console.log(res.data)
+    })
     return (
       <h2>Props</h2>
     )
