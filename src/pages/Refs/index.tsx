16f9e29403e36733f98e5b9a738d1a94a986dc4a
steviehao <gahao111@outlook.com>
2021-04-02 01:37:02 +0800

    [feat] Refs & DOM

--- src/pages/Refs/index.tsx
+++ src/pages/Refs/index.tsx
@@ -2,16 +2,24 @@
  * @Author: Stevie
  * @Date: 2021-03-31 06:05:27
  * @LastEditors: Stevie
- * @LastEditTime: 2021-03-31 07:12:11
+ * @LastEditTime: 2021-04-02 01:19:20
  * @Description: file content
  */
 import React from 'react';
+import { Divider } from 'antd'
+import StringRefs from './StringRefs';
+import CallbackRefs from './CallbackRefs';
+import CreateRefs from './CreateRefs';
 
 class RefsDemo extends React.Component {
     render() {
         return (
-            <div>
-
+            <div className="refs">
+                <StringRefs />
+                <Divider type="horizontal"></Divider>
+                <CallbackRefs />
+                <Divider type="horizontal"></Divider>
+                <CreateRefs />
             </div>
         );
     }
