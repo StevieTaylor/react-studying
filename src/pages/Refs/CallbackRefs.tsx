5c655b473aa6a5164c28533b144aff397f9b1b42
steviehao <gahao111@outlook.com>
2021-04-05 01:38:26 +0800

    回调形式的refs

--- src/pages/Refs/CallbackRefs.tsx
+++ src/pages/Refs/CallbackRefs.tsx
@@ -2,52 +2,73 @@
  * @Author: Stevie
  * @Date: 2021-04-02 00:24:45
  * @LastEditors: Stevie
- * @LastEditTime: 2021-04-02 01:31:08
+ * @LastEditTime: 2021-04-05 01:35:53
  * @Description: file content
  */
-import React from 'react';
-import { Button, Divider, Input, message } from 'antd';
+import React from 'react'
+import { Button, Col, Divider, Input, message, Row } from 'antd'
 
 class CallbackRefs extends React.Component {
-    inputElement: Input | undefined;
-    inputElement2: Input | undefined;
+  inputElement: Input | undefined
+  inputElement2: Input | undefined
 
-    showValue = () => {
-        message.info(this.inputElement?.state.value);
-    }
+  showValue = () => {
+    message.info(this.inputElement?.state.value)
+  }
 
-    showValue2 = () => {
-        message.info(this.inputElement2?.state.value);
-    }
+  showValue2 = () => {
+    message.info(this.inputElement2?.state.value)
+  }
 
-    // - React 会自动调用绑定在ref属性上的回调函数
-    refCallback = (element: Input) => {
-        this.inputElement2 = element;
-    }
+  // - React 会自动调用绑定在ref属性上的回调函数
+  refCallback = (element: Input) => {
+    this.inputElement2 = element
+  }
 
-    render() {
-        const style = {
-            display: 'inline',
-            width: 400,
-            marginRight: 5
-        }
-        return (
-            <div>
-                <h2>二、回调 Refs</h2>
-                <div>
-                    <h3>1.内联函数定义 ref 回调</h3>
-                    <Input placeholder="please input" style={style} ref={(el: Input) => this.inputElement = el} />
-                    <Button type="primary" onClick={this.showValue}>显示</Button>
-                </div>
-                <Divider type="horizontal"></Divider>
-                <div>
-                    <h3>2.class绑定函数定义 ref 回调</h3>
-                    <Input placeholder="please input" style={style} ref={this.refCallback} />
-                    <Button type="primary" onClick={this.showValue2}>显示</Button>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    const inputStyle = { minWidth: 400, maxWidth: 600 }
+    const paddingLeft_5 = { paddingLeft: 5 }
+    return (
+      <div>
+        <h2>二、回调 Refs</h2>
+        <div style={inputStyle}>
+          <h3>1. 内联函数定义 ref 回调</h3>
+          <Row>
+            <Col span={19}>
+              <Input
+                maxLength={400}
+                placeholder="请输入内容"
+                ref={(currentElement: Input) => {
+                  this.inputElement = currentElement
+                }}
+              />
+            </Col>
+            <Col span={5} style={paddingLeft_5}>
+              <Button type="primary" onClick={this.showValue}>
+                显示
+              </Button>
+            </Col>
+          </Row>
+        </div>
+
+        <Divider type="horizontal"></Divider>
+
+        <div style={inputStyle}>
+          <h3>2. class绑定函数定义 ref 回调</h3>
+          <Row>
+            <Col span={19}>
+              <Input placeholder="请输入内容" ref={this.refCallback} />
+            </Col>
+            <Col span={5} style={paddingLeft_5}>
+              <Button type="primary" onClick={this.showValue2}>
+                显示
+          </Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default CallbackRefs;
+export default CallbackRefs
