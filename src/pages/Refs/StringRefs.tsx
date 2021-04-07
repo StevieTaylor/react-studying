b2a1d6cffa02186a4c43470053e10204bfc9ed55
steviehao <gahao111@outlook.com>
2021-04-08 01:42:35 +0800

    [feat] String类型的refs

--- src/pages/Refs/StringRefs.tsx
+++ src/pages/Refs/StringRefs.tsx
@@ -2,11 +2,11 @@
  * @Author: Stevie
  * @Date: 2021-04-02 01:15:51
  * @LastEditors: Stevie
- * @LastEditTime: 2021-04-02 01:24:11
+ * @LastEditTime: 2021-04-08 01:39:48
  * @Description: file content
  */
 import React from 'react';
-import { Button, Input, message } from 'antd';
+import { Button, Col, Input, message, Row } from 'antd';
 
 class StringRefs extends React.Component {
 
@@ -17,16 +17,17 @@ class StringRefs extends React.Component {
     }
 
     render() {
-        const style = {
-            display: 'inline',
-            width: 400,
-            marginRight: 5
-        }
         return (
             <div>
                 <h2>一、String 类型的 Refs (已过时)</h2>
-                <Input placeholder="please input" style={style} ref="stringRef" />
-                <Button type="primary" onClick={this.showValue}>显示</Button>
+                <Row>
+                    <Col span={19}>
+                        <Input placeholder="请输入内容" ref="stringRef" />
+                    </Col>
+                    <Col span={5} className="paddingLeft5">
+                        <Button type="primary" onClick={this.showValue}>显示</Button>
+                    </Col>
+                </Row>
             </div>
         )
     }
