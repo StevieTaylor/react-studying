81b2e033878fc878ef386757cc581081aa2b3fd8
steviehao <gahao111@outlook.com>
2021-04-16 01:17:05 +0800

    [feat] React生命周期

---
+++ src/pages/Lifecycle/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-04-15 23:19:30
+ * @LastEditors: Stevie
+ * @LastEditTime: 2021-04-16 01:16:36
+ * @Description: file content
+ */
+import React from 'react';
+
+class LifeCycle extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default LifeCycle;
