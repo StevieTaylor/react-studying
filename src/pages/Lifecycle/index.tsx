fb3a95cc69a22a69d696217bcb9ccb89d404df24
hu hao <huhao@hus-MacBook-Pro.local>
2021-04-18 21:29:21 +0800

    [feat] React生命周期

--- src/pages/Lifecycle/index.tsx
+++ src/pages/Lifecycle/index.tsx
@@ -2,19 +2,33 @@
  * @Author: Stevie
  * @Date: 2021-04-15 23:19:30
  * @LastEditors: Stevie
- * @LastEditTime: 2021-04-16 01:16:36
+ * @LastEditTime: 2021-04-18 21:28:57
  * @Description: file content
  */
 import React from 'react';
+import { Tabs } from 'antd';
+import OldLifecycle from './OldLifecycle';
 
-class LifeCycle extends React.Component {
+const { TabPane } = Tabs;
+
+class ComponentLifeCycle extends React.Component {
+
+  onTabChange = (key: string) => {
+    console.log('key :>> ', key);
+  }
+
   render() {
     return (
-      <div>
-
-      </div>
+      <Tabs type="card" onChange={this.onTabChange}>
+        <TabPane tab="旧版生命周期" key="oldLifecycle">
+          <OldLifecycle />
+        </TabPane>
+        <TabPane tab="新版生命周期" key="newLifecycle">
+          新版生命周期
+        </TabPane>
+      </Tabs>
     );
   }
 }
 
-export default LifeCycle;
+export default ComponentLifeCycle;
