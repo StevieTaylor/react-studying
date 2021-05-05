a807b62732b202fda9d722302f2a355d1f27cff3
hu hao <huhao@hus-MacBook-Pro.local>
2021-05-05 15:58:29 +0800

    [feat] 新版生命周期的子组件

---
+++ src/pages/Lifecycle/NewChildComponent.tsx
@@ -0,0 +1,24 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-05-03 17:49:01
+ * @LastEditTime: 2021-05-03 17:51:17
+ * @LastEditors: Stevie
+ * @Description:
+ */
+import React, { Component } from 'react';
+
+interface IProp {
+  time: number;
+}
+
+class NewChildComponent extends Component<IProp, any> {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default NewChildComponent;
