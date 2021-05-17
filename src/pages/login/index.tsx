996d7669fceb569d2ff0c3158be521712b2eb031
hu hao <stevieforwork@126.com>
2021-05-17 13:50:43 +0800

    [feat] 登陆页初始化

---
+++ src/pages/login/index.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-03-18 12:39:39
+ * @LastEditTime: 2021-05-17 13:47:44
+ * @LastEditors: Stevie
+ * @Description: 
+ */
+import * as React from "react";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div className="login">
+                <h1>登陆页</h1>
+            </div>
+        )
+    }
+}
+
+export default Login
