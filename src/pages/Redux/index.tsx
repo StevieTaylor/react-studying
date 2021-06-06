5cf138915f61ededc3b69999ca3b363dff63bbea
StevieTaylor <galei111@126.com>
2021-06-06 17:13:27 +0800

    [feat] Redux

--- src/pages/Redux/index.tsx
+++ src/pages/Redux/index.tsx
@@ -1,19 +1,25 @@
 /*
  * @Author: Stevie
  * @Date: 2021-04-29 17:17:21
- * @LastEditTime: 2021-05-09 18:03:36
+ * @LastEditTime: 2021-06-06 17:11:54
  * @LastEditors: Stevie
  * @Description:
  */
-import React from 'react';
-import SumDemo from './components/SumDemo';
-import './style.less';
+import * as React from 'react';
+import { Image } from 'antd';
+import ReactCalculator from './components/ReactCalculator';
 
 class ReduxDemo extends React.Component {
   render() {
     return (
       <div className="redux-demo-container">
-        <SumDemo />
+        <h2>一、Redux原理图</h2>
+        <Image
+          width={500}
+          src={require('../../assets/images/redux.png').default}
+        />
+        <h2>二、Calculator Demo</h2>
+        <ReactCalculator />
       </div>
     );
   }
