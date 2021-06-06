76ad74c1f975e53ef0b0e5e3ccd700090834716c
StevieTaylor <galei111@126.com>
2021-06-06 17:20:18 +0800

    [refactor] 调整redux文件位置

---
+++ src/pages/Redux/ReduxCalculator/ReduxCalculator.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-06-06 17:14:31
+ * @LastEditTime: 2021-06-06 17:14:58
+ * @LastEditors: Stevie
+ * @Description: Redux版简易计算器
+ */
+import * as React from 'react';
+
+class ReduxCalculator extends React.Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default ReduxCalculator;
--- src/pages/Redux/index.tsx
+++ src/pages/Redux/index.tsx
@@ -1,13 +1,13 @@
 /*
  * @Author: Stevie
  * @Date: 2021-04-29 17:17:21
- * @LastEditTime: 2021-06-06 17:11:54
+ * @LastEditTime: 2021-06-06 17:18:55
  * @LastEditors: Stevie
  * @Description:
  */
 import * as React from 'react';
 import { Image } from 'antd';
-import ReactCalculator from './components/ReactCalculator';
+import ReactCalculator from './ReactCalculator';
 
 class ReduxDemo extends React.Component {
   render() {
