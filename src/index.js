91cadbc1102eb4a517d70c93e30da18594e86c3f
StevieTaylor <galei111@126.com>
2021-06-06 22:34:56 +0800

    [feat] 主入口文件

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import ReactDOM from 'react-dom'
-import App from './App'
+/*
+ * @Author: Stevie
+ * @Date: 2021-05-31 21:09:59
+ * @LastEditTime: 2021-06-06 21:16:44
+ * @LastEditors: Stevie
+ * @Description: 主入口文件
+ */
+import ReactDOM from 'react-dom';
+import App from './App';
 import 'antd/dist/antd.css';
-import './styles/index.less'
+import './styles/index.less';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
