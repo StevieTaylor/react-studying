0ffd4397268f28c5c2a19dd2eeb9e3f4152aa346
StevieTaylor <galei111@126.com>
2021-06-11 16:15:32 +0800

    [feat] actions

--- src/store/actions/calcalator.ts
+++ src/store/actions/calcalator.ts
@@ -1,23 +1,20 @@
 /*
  * @Author: Stevie
  * @Date: 2021-06-06 21:46:07
- * @LastEditTime: 2021-06-09 00:12:04
+ * @LastEditTime: 2021-06-11 16:14:50
  * @LastEditors: Stevie
  * @Description:
  */
 import { CalculatorActionTypes as types } from '../action-types';
 
-// - 同步action: action对象为普通对象
-const CalculatorActions = {
+export const CalculatorActions = {
+  // - 同步action: action对象为普通对象
   add: (data: any) => ({ type: types.ADD, data }),
   subtract: (data: any) => ({ type: types.SUBTRACT, data }),
   multiply: (data: any) => ({ type: types.MULTIPLY, data }),
   divide: (data: any) => ({ type: types.DIVIDE, data }),
-  reset: (data: any) => ({ type: types.RESET, data })
-};
-
-// - 异步action: action对象为函数
-export const CalculatorAsyncActions = {
+  reset: (data: any) => ({ type: types.RESET, data }),
+  // - 异步action: action对象为函数
   resetAfterOneSecond: (data: any, timeout: number) => {
     return (dispatch) => {
       setTimeout(() => {
@@ -25,6 +22,4 @@ export const CalculatorAsyncActions = {
       }, timeout);
     }
   }
-}
-
-export default CalculatorActions;
+};
---
+++ src/store/actions/index.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-06-11 16:01:25
+ * @LastEditTime: 2021-06-11 16:03:40
+ * @LastEditors: Stevie
+ * @Description: all actions
+ */
+import { CalculatorActions } from './calcalator';
+
+export {
+  CalculatorActions,
+}
