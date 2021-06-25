7ba53d61fdf187f65de8e9135711f6aa78b980a5
StevieTaylor <galei111@126.com>
2021-06-25 17:29:27 +0800

    [feat] useEffect

---
+++ src/pages/ReactHooks/components/UseEffect.tsx
@@ -0,0 +1,20 @@
+/*
+ * @Author: Stevie
+ * @Date: 2021-06-25 10:13:47
+ * @LastEditTime: 2021-06-25 17:20:54
+ * @LastEditors: Stevie
+ * @Description:
+ */
+import * as React from 'react'
+import { Divider } from 'antd';
+
+const UseEffect: React.FC = () => {
+  return (
+    <div>
+      <h2>二、useEffect</h2>
+      <h4 className="h4-ref">组件更新副作用钩子</h4>
+      <Divider type="horizontal"></Divider>
+    </div>
+  )
+}
+export default UseEffect;
