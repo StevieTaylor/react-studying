86a07b388bc8726bc5fa96f6429b2a91d141e81c
StevieTaylor <galei111@126.com>
2021-08-08 23:38:28 +0800

    [feat] github search static page

--- src/pages/Network/components/ResultList.tsx
+++ src/pages/Network/components/ResultList.tsx
@@ -1,20 +1,60 @@
 /*
  * @Author: Stevie
  * @Date: 2021-08-08 22:40:20
- * @LastEditTime: 2021-08-08 23:00:37
+ * @LastEditTime: 2021-08-08 23:28:03
  * @LastEditors: Stevie
  * @Description:
  */
-import * as React from 'react';
-
+import * as React from 'react'
+import { Card } from 'antd'
+const { Meta } = Card
 class ResultList extends React.Component {
+  mockUsers = [
+    {
+      alt: 'username',
+      imgSrc: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      title: 'username',
+      description: 'react'
+    },
+    {
+      alt: 'username',
+      imgSrc: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      title: 'username',
+      description: 'react'
+    },
+    {
+      alt: 'username',
+      imgSrc: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      title: 'username',
+      description: 'react'
+    },
+    {
+      alt: 'username',
+      imgSrc: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      title: 'username',
+      description: 'react'
+    },
+    {
+      alt: 'username',
+      imgSrc: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png',
+      title: 'username',
+      description: 'react'
+    }
+  ]
+
   render() {
     return (
       <div className="result-list">
-        结果
+        {this.mockUsers.map((user) => {
+          return (
+            <Card hoverable className="user-card" cover={<img alt={user.alt} src={user.imgSrc} />}>
+              <Meta title={user.title} description={user.description}></Meta>
+            </Card>
+          )
+        })}
       </div>
-    );
+    )
   }
 }
 
-export default ResultList;
+export default ResultList
