b2a7ff5d7178ad4a8b987dab65c65671cebcf690
StevieTaylor <galei111@126.com>
2022-01-24 17:37:30 +0800

    [feat] 封装useWindowSize

--- src/common/hooks/index.ts
+++ src/common/hooks/index.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: Stevie
  * @Date: 2021-11-15 14:40:26
- * @LastEditTime: 2021-11-15 14:41:54
+ * @LastEditTime: 2022-01-24 16:40:42
  * @LastEditors: Stevie
  * @Description: 
  */
 import useWindowSize from './useWindowSize'
 
-export default useWindowSize
+export { useWindowSize }
--- src/common/hooks/useWindowSize.ts
+++ src/common/hooks/useWindowSize.ts
@@ -1,10 +1,41 @@
 /*
  * @Author: Stevie
  * @Date: 2021-11-15 14:40:45
- * @LastEditTime: 2021-11-15 14:40:46
+ * @LastEditTime: 2022-01-24 17:18:29
  * @LastEditors: Stevie
- * @Description: 
+ * @Description:
  */
-const aaa = 1
+import * as React from 'react'
 
-export default aaa;
+export interface WindowSize {
+  width: number | undefined
+  height: number | undefined
+}
+
+function useWindowSize(): WindowSize {
+  const [windowSize, setWindowSize] = React.useState<WindowSize>({
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
+  React.useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight
+      })
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    // handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  return windowSize
+}
+
+export default useWindowSize
